14b5216115cf7d7ba8c57abbdd2f0785491c2aa6
wanghao <776848506@qq.com>
2019-07-25 16:47:49 +0800

    测试

--- src/com/woniuxy/test/Test.java
+++ src/com/woniuxy/test/Test.java
@@ -1,7 +1,15 @@
 package com.woniuxy.test;
 
+/**
+ * 打开eclipse之后
+ * 1.从远程仓库上拉取代码
+ * 2.在自己的分支上编写代码
+ * 3.测试没有问题在提交自己的代码到自己的分支上
+ * 4.在下班之前，应该将自己分支上的代码合并到主干master上
+ */
 public class Test {
     public static void main(String[] args) {
         System.out.println("hell git");
+        System.out.println("");
     }
 }
