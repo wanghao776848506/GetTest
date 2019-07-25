89bced8b00d976dc1d0d6fa797f444f69906aa9c
wanghao <776848506@qq.com>
2019-07-25 17:23:57 +0800

    测试

--- src/com/woniuxy/test/Test.java
+++ src/com/woniuxy/test/Test.java
@@ -6,10 +6,11 @@ package com.woniuxy.test;
  * 2.在自己的分支上编写代码
  * 3.测试没有问题在提交自己的代码到自己的分支上
  * 4.在下班之前，应该将自己分支上的代码合并到主干master上
+ *
  */
 public class Test {
     public static void main(String[] args) {
         System.out.println("hell git");
-        System.out.println("");
+        System.out.println("这是测试代码2.0");
     }
 }
